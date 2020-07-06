6f1a1bcd105be7f3926a43a94135f116753e4bb9
Tom Doan <thdoan@gmail.com>
2020-07-05 22:29:36 -0700

    Delete unused files

--- src/extern1.js
+++
@@ -1,9 +0,0 @@
-var g_wordmap;
-var g_defs;
-var g_wstr;
-var g_letters;
-var g_vowels;
-var g_letrange;
-function t( str ) {}
-function logit( str ) {}
-function Sizzle( selector, context, results, seed ) {}
