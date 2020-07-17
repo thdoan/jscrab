ffed6289719645e92418fad427b80b70bcb5dbdb
Tom Doan <thdoan@gmail.com>
2020-07-16 20:04:04 -0700

    Don't prompt in build script

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ set target=play
 AjaxMin.exe -clobber css\style.css css\submodal.css -out %target%\css\styles.min.css
 AjaxMin.exe -inline:no -clobber -term lang\vi_wordlist.js lang\vi_defs.js lang\vi_letters.js src\submodal.js src\sizzle.js src\redipsdrag.js src\bonuses.js src\ui.js src\engine.js -out %target%\js\app.min.js
 copy /y index.html %target%
-xcopy lang\en_translate.js %target%\lang\
-xcopy lang\vi_translate.js %target%\lang\
+xcopy /y lang\en_translate.js %target%\lang\
+xcopy /y lang\vi_translate.js %target%\lang\
 xcopy /iy pics %target%\pics
 xcopy /iy sounds %target%\sounds
 fart -C %target%\index.html --remove "<link rel=\"stylesheet\" href=\"css/style.css\">\n"
