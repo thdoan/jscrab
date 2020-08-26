1ba2b9ffb35ec9728ce96e3976786bd40296604e
Tom Doan <thdoan@gmail.com>
2020-08-25 17:04:19 -0700

    Fix incorrect final scores in right column

--- src/changelog.js
+++ src/changelog.js
@@ -1,15 +1,10 @@
-const VER = '1.11';
+const VER = '1.12';
 
 function showWhatsNew() {
   g_bui.prompt(
     '<h3>What\'s New</h3>' +
     '<ul>' +
-    '<li>Fixed Vietnamese text wrapping on iPad</li>' +
-    '<li>Added Flower Garden bonuses layout</li>' +
-    '<li>Updated desktop logo to be consistent with mobile</li>' +
-    '<li>Moved desktop logo to the left side</li>' +
-    '<li>Added High Scores button (placeholder)</li>' +
-    '<li>Added version number</li>' +
+    '<li>Fixed incorrect final scores in game info column</li>' +
     '</ul>'
   );
 }
