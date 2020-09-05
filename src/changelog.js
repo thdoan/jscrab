1d8df3e7328884f3a289f9bdfe64f8538ddaff89
Tom Doan <thdoan@gmail.com>
2020-09-04 19:53:53 -0700

    Increase space tile value from 1 to 10 points

--- src/changelog.js
+++ src/changelog.js
@@ -1,10 +1,11 @@
-const VER = '1.14';
+const VER = '1.15';
 
 function showWhatsNew() {
   g_bui.prompt(
     '<h3>What\'s New</h3>' +
     '<ul>' +
     '<li>You can now play part of a compound word</li>' +
+    '<li>Increased space tile value from 1 to 10 points</li>' +
     '</ul>'
   );
 }
