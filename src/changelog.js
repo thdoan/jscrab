702407637b8485efb1ae0945fa7cf94af0bc665c
Tom Doan <thdoan@gmail.com>
2020-09-15 00:59:26 -0700

    Fix session not saved if computer passes

--- src/changelog.js
+++ src/changelog.js
@@ -1,11 +1,12 @@
-const VER = '1.24';
+const VER = '1.25';
 
 function showWhatsNew() {
   g_bui.prompt(
     '<h3>What\'s New</h3>' +
     '<ul>' +
-    '<li>Fixed scoreboard not updated on game over</li>' +
+    '<li>Fixed mobile scoreboard not updated on game over</li>' +
     '<li>Now showing player score first</li>' +
+    '<li>Fixed session not saved if computer passes</li>' +
     '</ul>'
   );
 }
